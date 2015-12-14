986e6eabf60c2243da5da019fa81df2a4fa7a439
Toni <afluriach@gmail.com>
2015-12-14 06:48:42 -0500

    de-inline Lua::Class methods

---
+++ Classes/LuaWrap.cpp
@@ -0,0 +1,81 @@
+//
+//  LuaWrap.cpp
+//  FlansBasement
+//
+//  Created by Toni on 12/14/15.
+//
+//
+
+#include "Prefix.h"
+
+#include "LuaWrap.h"
+
+namespace Lua{
+
+void Class::installClass(lua_State* L)
+{
+    makeMetatable(L);
+    
+    //Add all wrapped methods
+    
+    //Get metatable
+    lua_getglobal(L, name.c_str());
+
+    foreach(auto methodEntry, methods){
+        string name = methodEntry.first;
+        lua_CFunction wrapper = methodEntry.second;
+        
+        //Add method
+        
+        //for table assign, value is top of stack and key is second.
+        lua_pushstring(L, name.c_str());
+        lua_pushcfunction(L, wrapper);
+        
+        lua_rawset(L, -3);
+    }
+}
+
+void Class::makeMetatable(lua_State* L)
+{
+    //check if it already exists, which shouldn't happen unless the
+    //create constructor was inappopriately invoked.
+
+    lua_newtable(L);
+    lua_setglobal(L,name.c_str());
+    
+    //Set __index
+    lua_getglobal(L,name.c_str());
+    lua_pushstring(L, "__index");
+    lua_getglobal(L,name.c_str());
+    lua_rawset(L, -3);
+}
+
+//statics
+
+void Class::setMetatable(const string& name, lua_State* L)
+{
+    if(!lua_istable(L, -1))
+        throw runtime_error("setMetatable: top of stack must table.");
+    
+    lua_getglobal(L, name.c_str());
+    
+    if(lua_isnil(L, -1))
+        throw runtime_error(name + " metatable not found.");
+    
+    lua_setmetatable(L, -2);
+}
+
+void Class::installClasses(lua_State* L)
+{
+    if(!init){
+        makeClasses();
+        init = true;
+    }
+
+    foreach(auto classEntry, classes){
+        classEntry.second.installClass(L);
+    }
+}
+
+
+}
--- Classes/LuaWrap.h
+++ Classes/LuaWrap.h
@@ -21,18 +21,8 @@ private:
     unordered_map<string, lua_CFunction> methods;
 public:
     static void makeClasses();
-    
-    inline static void installClasses(lua_State* L)
-    {
-        if(!init){
-            makeClasses();
-            init = true;
-        }
-    
-        foreach(auto classEntry, classes){
-            classEntry.second.installClass(L);
-        }
-    }
+    static void installClasses(lua_State* L);
+    static void setMetatable(const string& name, lua_State* L);
     
     inline static void makeClass(string name)
     {
@@ -60,56 +50,9 @@ public:
     {
     }
     
-    inline void installClass(lua_State* L)
-    {
-        makeMetatable(L);
-        
-        //Add all wrapped methods
-        
-        //Get metatable
-        lua_getglobal(L, name.c_str());
-
-        foreach(auto methodEntry, methods){
-            string name = methodEntry.first;
-            lua_CFunction wrapper = methodEntry.second;
-            
-            //Add method
-            
-            //for table assign, value is top of stack and key is second.
-            lua_pushstring(L, name.c_str());
-            lua_pushcfunction(L, wrapper);
-            
-            lua_rawset(L, -3);
-        }
-    }
+    void installClass(lua_State* L);
+    void makeMetatable(lua_State* L);
     
-    inline void makeMetatable(lua_State* L)
-    {
-        //check if it already exists, which shouldn't happen unless the
-        //create constructor was inappopriately invoked.
-
-        lua_newtable(L);
-        lua_setglobal(L,name.c_str());
-        
-        //Set __index
-        lua_getglobal(L,name.c_str());
-        lua_pushstring(L, "__index");
-        lua_getglobal(L,name.c_str());
-        lua_rawset(L, -3);
-    }
-    
-    static inline void setMetatable(const string& name, lua_State* L)
-    {
-        if(!lua_istable(L, -1))
-            throw runtime_error("setMetatable: top of stack must table.");
-        
-        lua_getglobal(L, name.c_str());
-        
-        if(lua_isnil(L, -1))
-            throw runtime_error(name + " metatable not found.");
-        
-        lua_setmetatable(L, -2);
-    }
 };
 
 //conversion helper
