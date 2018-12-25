39ff3f39a743bdb2d98ee9551fe44b3ba8765862
afluriach <afluriach@gmail.com>
2018-12-24 20:33:07 -0700

    Desert updates - change D1 pyramid movement pattern to continuous.

--- Classes/scenes/Desert.cpp
+++ Classes/scenes/Desert.cpp
@@ -16,7 +16,8 @@ const vector<GScene::MapEntry> Desert::rooms = {
 	{make_pair("desert/D2", getRoomOffset(roomSize, 0, 0))},
 	{make_pair("desert/D0", getRoomOffset(roomSize, 1, 0))},
 	{make_pair("desert/D1", getRoomOffset(roomSize, 2, 0))},
-	{make_pair("desert/D3", getRoomOffset(roomSize, 3, 0))}
+	{make_pair("desert/D3", getRoomOffset(roomSize, 3, 0))},
+	{make_pair("desert/D4", getRoomOffset(roomSize, 4, 0))}
 };
 
 Desert::Desert() :
