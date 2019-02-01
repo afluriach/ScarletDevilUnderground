0c1afe132e61bf34970034b94c8b0b6f93abf90e
afluriach <afluriach@gmail.com>
2019-02-01 08:48:44 -0700

    Add forest F4-F7
    Resize/reshape F0 to have a corridor at the north

--- Classes/scenes/Forest.cpp
+++ Classes/scenes/Forest.cpp
@@ -13,10 +13,14 @@
 const IntVec2 Forest::roomSize = IntVec2(17,17);
 
 const vector<GScene::MapEntry> Forest::rooms = {
-	{make_pair("forest/F3", IntVec2(0,1))},
 	{make_pair("forest/F0", IntVec2(53,16))},
 	{make_pair("forest/F1", IntVec2(70,16))},
 	{make_pair("forest/F2", IntVec2(87,16))},
+	{make_pair("forest/F3", IntVec2(0,1))},
+	{make_pair("forest/F4", IntVec2(39,39))},
+	{make_pair("forest/F5", IntVec2(0,38))},
+	{make_pair("forest/F6", IntVec2(11,77))},
+	{make_pair("forest/F7", IntVec2(11,98))},
 };
 
 Forest::Forest() :
